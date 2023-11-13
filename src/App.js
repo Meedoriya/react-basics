9cb76a4bf23982c98432e240edefb573ec89a35f
Meedoriya <meedoriya@gmail.com>
2023-11-13 19:08:05 +0600

    creating and importing components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
+import Heading from "./components/Heading"
 
-
-function Heading() {
-  return (
-  <h1>This is an h1 heading</h1>
-  )
-}
 function App() {
   return (
     <div class="App">
-      This is the starting code for "Your first component" ungraded lab
       <Heading />
     </div>
   )
